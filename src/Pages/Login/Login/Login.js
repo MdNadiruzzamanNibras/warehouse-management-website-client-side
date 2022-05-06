1ba69a9b732fd394dc0310251dc7c6c25b74e31c
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-06 09:48:04 +0600

    login from create

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -8,9 +8,20 @@ const Login = () => {
         loading,
         error,
       ] = useSignInWithEmailAndPassword(auth);
+
+    const handleLogin =event=>{
+        event.preventDefault();
+        const email= event.target.email.value;
+        const password= event.target.password.value;
+        signInWithEmailAndPassword(email, password)
+    }
     return (
         <div>
-            
+           <form onSubmit={handleLogin}>
+           <input type="text" name="email" id="" className='w-50 my-2 mx-auto py-2 d-block' />
+           <input type="password" name="password" id="" className='w-50 my-2 py-2 mx-auto d-block' />
+           <input type="submit" className='mx-auto d-block' value="Login" />
+           </form>
         </div>
     );
 };
