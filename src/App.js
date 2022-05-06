13216d488f4665edf2dfc241a1687798bc73b59e
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-06 09:32:07 +0600

    sign in react firebase hook

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home/Home/Home';
 import Inventories from './Pages/Home/Inventory/Inverntories/Inventories';
+import Login from './Pages/Login/Login/Login';
 import ManageInventories from './Pages/Manage Inventory/Manage Inventories/ManageInventories';
 import Header from './Pages/Shared/Header/Header';
-import Login from './Pages/Shared/Login/Login';
+
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
        <Route path='/' element={<Home></Home>}></Route>
        <Route path='/inventory' element={<Inventories></Inventories>}></Route>
        <Route path='/inventory/:inventoryId'></Route>
-       <Route path='/login' element={<Login></Login>}></Route>
+       <Route path='/login' element={<Login></Login>} ></Route>
        <Route path='/manageitem' element={<ManageInventories></ManageInventories>}></Route>
      </Routes>
     </div>
