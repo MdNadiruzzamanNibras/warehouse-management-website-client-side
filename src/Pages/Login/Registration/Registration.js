aaacc919de4d90d79426a7fe1c6cba87fcd36347
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-06 11:58:14 +0600

    registration

--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Registration = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+      const handleRegistration = event=>{
+          event.preventDefault();
+          const name = event.target.name.value;
+          const email = event.target.email.value;
+          const password = event.target.password.value;
+          console.log(name, email, password)
+          createUserWithEmailAndPassword(email, password)
+      }
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleRegistration}>
             <input type="text" name="name" id="" className='w-50 my-2 mx-auto py-2 d-block' />
             <input type="text" name="email" id="" className='w-50 my-2 mx-auto py-2 d-block' />
             <input type="password" name="password" id="" className='w-50 my-2 mx-auto py-2 d-block' />
