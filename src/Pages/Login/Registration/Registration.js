481eb90ede08a81c9bbbff0aee7caf64244b9ace
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-06 09:56:36 +0600

    registration form create

--- src/Pages/Login/Registration/Registration.js
+++ src/Pages/Login/Registration/Registration.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Registration = () => {
     return (
         <div>
-            
+            <form>
+            <input type="text" name="name" id="" className='w-50 my-2 mx-auto py-2 d-block' />
+            <input type="text" name="email" id="" className='w-50 my-2 mx-auto py-2 d-block' />
+            <input type="password" name="password" id="" className='w-50 my-2 mx-auto py-2 d-block' />
+           
+            </form>
         </div>
     );
 };
