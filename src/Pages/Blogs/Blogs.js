0f10d8633849a54c535f59e2017e3087d04ff408
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-08 17:41:12 +0600

    requireauth

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -9,6 +9,8 @@ const Blogs = () => {
 
                 <h2>When should you use nodejs and when should you use mongodb?</h2>
                 <p>When we used javascript fonted then we can used backend for nodejs. As nodejs  is a bigest open source platform. It has npm packages is 65000 code free source. When you used NO-SQL database you must used mongodb. It's  a safe  No-SQL database and It is popular database.</p>
+                <h2>Differences between sql and nosql databases.</h2>
+                <p>sql is a call realtion database and nosql is call no-realtion database. sql is used query language and have schema other hand nosql is used dynamic schema and unstructured data. sql is vertically scalable and nosql is horizontally scalable. sql database is tablebase and nosql is document, key-value, graph and wide-column stores. </p>
             </div>
         </div>
     );
