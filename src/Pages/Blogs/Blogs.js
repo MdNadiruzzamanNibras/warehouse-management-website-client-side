004a4aaa7293140add7db6518b910d32dad4cbd1
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-08 17:27:08 +0600

    second ques done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -6,6 +6,9 @@ const Blogs = () => {
             <div className="row">
                 <h2> Difference between javascript and nodejs?</h2>
                 <p>Javascript and nodejs main difference  javascript is  run client site and nodejs is run server site. Javascript is used for writing scripts on the website and nodejs used to javascript runing. Javascript is capable to HTML play dom but node is not capable. Javascript is used just frontend development other hand nodejs used server site development. Javascript used framework is  RamdaJS, TypedJS etc and nodejs used framework Lodash, express etc. </p>
+
+                <h2>When should you use nodejs and when should you use mongodb?</h2>
+                <p>When we used javascript fonted then we can used backend for nodejs. As nodejs  is a bigest open source platform. It has npm packages is 65000 code free source. When you used NO-SQL database you must used mongodb. It's  a safe  No-SQL database and It is popular database.</p>
             </div>
         </div>
     );
