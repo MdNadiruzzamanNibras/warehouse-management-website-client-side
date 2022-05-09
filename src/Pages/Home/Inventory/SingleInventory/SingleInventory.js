86789acc34a69692362a77aed6aa7999eae40fbc
MdNadiruzzamanNibras <nadiruzzamannibras@gmail.com>
2022-05-09 18:13:46 +0600

    restock

--- src/Pages/Home/Inventory/SingleInventory/SingleInventory.js
+++ src/Pages/Home/Inventory/SingleInventory/SingleInventory.js
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const SingleInventory = () => {
-    const restockRef =  useRef()
+    // const restockRef =  useRef()
     const {inventoryId} = useParams()
     const [inventory, setInvertory] = useState({})
     useEffect(()=>{
@@ -21,7 +21,7 @@ const SingleInventory = () => {
            console.log(newQuauntity);
            if(newQuauntity){
                console.log('its work');
-               const url =`http://localhost:5000/inventory/${id}`
+               const url =`https://sleepy-citadel-14654.herokuapp.com/inventory/${id}`
                fetch(url,{
                    method: 'PUT',
                    headers:{
@@ -36,34 +36,35 @@ const SingleInventory = () => {
            }
 
        }
-       const handleplus = id=>{
-           const Quantity= inventory.quantity
-           const restock = restockRef.current.value 
-           const restockpars = parseInt(restock)
-           console.log(restockpars);
-           const newQuauntity = Quantity + restockpars
-           console.log(newQuauntity);
-           if(newQuauntity){
-               const url =`https://sleepy-citadel-14654.herokuapp.com/inventory/${id}`
-               fetch(url,{
-                   method: 'PUT',
-                   headers:{
-                    'content-type':'application/json'
-                   },
-                   body: JSON.stringify({quantity: newQuauntity})
+    //    const handleplus = id=>{
+    //        const Quantity= inventory.quantity
+    //        const restock = restockRef.current.value 
+    //        const restockpars = parseInt(restock)
+    //        console.log(restockpars);
+    //        const newQuauntity = Quantity + restockpars
+    //        console.log(newQuauntity);
+    //        if(newQuauntity){
+    //            const url =`https://sleepy-citadel-14654.herokuapp.com/inventory/${id}`
+    //            fetch(url,{
+    //                method: 'PUT',
+    //                headers:{
+    //                 'content-type':'application/json'
+    //                },
+    //                body: JSON.stringify({quantity: newQuauntity})
                   
-               })
+    //            })
              
-               .then(res=>res.json())
-               .then(data=> {setInvertory(data)
-               console.log(data)})
-           }
+    //            .then(res=>res.json())
+    //            .then(data=> {setInvertory(data)
+    //            console.log(data)})
+    //        }
 
-       }
+    //    }
     return (
         <div key={inventory._id}>
             <div className="container">
-                <div className="row" key={inventory._id}>
+                <div className="row">
+                <div key={inventory._id}>
                     <div>
                         <img src={inventory.img} alt="" />
                     </div>
@@ -76,10 +77,11 @@ const SingleInventory = () => {
                         <p><small>{inventory.description}</small></p>
                         <h6>Supplier:{inventory.supplier}</h6>
                         <button onClick={()=>handleDel(inventory._id)}>delivered</button>
-                        <input type="number" ref={restockRef} />
-                        <button onClick={handleplus}>Restok</button>
+                        {/* <input type="number" ref={restockRef} />
+                        <button onClick={handleplus}>Restok</button> */}
                     </div>
                     
+                </div>
                 </div>
             </div>
         </div>
